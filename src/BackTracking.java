7609a5e94d3970f079a5ef4948a011d1d15d2226
Srrijan Arora <sabhaya5567@gmail.com>
2025-04-07 03:56:09 +0530

    Added an optimized version of permute function

--- src/BackTracking.java
+++ src/BackTracking.java
@@ -1,2 +1,20 @@
 public class BackTracking {
+    public static void main(String[] args) {
+        permutation(new StringBuilder("abcd"),new StringBuilder(),0);
+    }
+    // method to print all possible combinations of a string using backtracking
+    static void permutation(StringBuilder str,StringBuilder perm,int idx){
+        if(str.isEmpty()){
+            System.out.println(perm);
+            return;
+        }
+        for(int i=0;i<str.length();i++){
+            char currentChar=str.charAt(i);
+            StringBuilder newStr= new StringBuilder(str);
+            newStr.deleteCharAt(i);
+            perm.append(currentChar);
+            permutation(newStr,perm,idx+1);
+            perm.deleteCharAt(perm.length() - 1);
+        }
+    }
 }
